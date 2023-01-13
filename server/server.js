44db3452383b3a066dafbf29b94b88cba32d81f3
Trenton Dean <trentondean@icloud.com>
2023-01-13 11:36:53 -0600

    updated server file

--- server/server.js
+++ server/server.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = 8000;
-    
+
+require('./config/mongoose.config');
+
+app.use(cors({origin:"http://localhost:3000"}))
+app.use(express.json());                           
+app.use(express.urlencoded({ extended: true }));
+
+const theScoopRoutes = require("./routes/theScoop.routes");
+theScoopRoutes(app);
+
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
